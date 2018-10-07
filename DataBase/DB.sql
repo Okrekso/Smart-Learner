c30953cdbdca97d2dedae7d34ad0d10e2fbf30a4
MykolaCompany <36553905+MykolaCompany@users.noreply.github.com>
2018-10-07 23:37:34 +0300

    Update DataBase/DB.sql
    Add FOREIGN KEY + CHECK in Subjects

--- DataBase/DB.sql
+++ DataBase/DB.sql
@@ -8,7 +8,8 @@ CREATE TABLE `Users`(
 	`SecondName` 	VARCHAR(50) NOT NULL DEFAULT "SecondName",
 	`PriorityLevel`	INT 		NOT NULL DEFAULT 2 CHECK(PriorityLevel >= 0 AND PriorityLevel <= 4),
     `Birthday`		DATETIME,
-    `GroupID`		INT
+    `GroupID`		INT,
+    FOREIGN KEY (`GroupID`) REFERENCES `Group`(`Id`) 
 );
 
 /*PriorityLevel - 0 - Admin, 1 - Techer, 2 - Student, 3 - Jast User */
@@ -17,13 +18,16 @@ CREATE TABLE `Points`(
 	`Id`		INT AUTO_INCREMENT PRIMARY KEY,
 	`Value` 	INT NOT NULL DEFAULT 0,
     `UserId`  	INT NOT NULL,
-    `SubjectId`	INT NOT NULL
+    `SubjectId`	INT NOT NULL,
+    FOREIGN KEY (`UserID`) REFERENCES `Users`(`Id`), 
+    FOREIGN KEY (`SubjectId`) REFERENCES `Subjects`(`Id`)     
 );
 
 CREATE TABLE `Subjects`(
 	`Id`		INT AUTO_INCREMENT PRIMARY KEY,
 	`Name`		VARCHAR(50) NOT NULL DEFAULT "Name",
-	`TeacherId`	INT NOT NULL
+	`TeacherId`	INT NOT NULL CHECK((SELECT `PriorityLevel` FROM `Users` WHERE `Id` = `TeacherId`) = 1),/*1 - Techer*/
+    FOREIGN KEY (`TeacherId`) REFERENCES `Users`(`Id`)     
 );
 
 CREATE TABLE `Groups`(
@@ -35,13 +39,16 @@ CREATE TABLE `News`(
 	`Id`		INT AUTO_INCREMENT PRIMARY KEY,
     `Header` 	VARCHAR(100) NOT NULL,
     `Content` 	VARCHAR(500) NOT NULL,
-    `CreatorId`	INT
+    `CreatorId`	INT,
+    FOREIGN KEY (`CreatorId`) REFERENCES `Users`(`Id`)
 );
 
 CREATE TABLE `Visitations`(
 	`Id`		INT AUTO_INCREMENT PRIMARY KEY,
     `UserId`	INT NOT NULL,
-    `SubjectId`	INT
+    `SubjectId`	INT,
+    FOREIGN KEY (`UserId`) REFERENCES `Users`(`Id`),
+    FOREIGN KEY (`SubjectId`) REFERENCES `Subjects`(`Id`)
 );
 
 CREATE TABLE `Schedules`(
