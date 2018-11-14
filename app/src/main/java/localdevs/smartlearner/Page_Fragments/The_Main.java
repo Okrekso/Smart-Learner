8bac202c1c7bc529cbc45c4ae2b3da416dbbb985
MykolaCompany <36553905+MykolaCompany@users.noreply.github.com>
2018-11-14 13:15:44 +0200

    Updata
    Add:
        new menu,
        new icon app,
        new login Page

---
+++ app/src/main/java/localdevs/smartlearner/Page_Fragments/The_Main.java
@@ -0,0 +1,18 @@
+package localdevs.smartlearner.Page_Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import localdevs.smartlearner.R;
+
+public class The_Main extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_the__main, container, false);
+
+    }
+}
